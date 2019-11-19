6feecef964d0d601e60494ca426c36f464fb5ff6
louiszhenyean <louiszhenyean@gmail.com>
2019-11-19 16:49:39 +0800

    whatever nah~

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # myDesktop
-Just some Desktop and icon configuration, my favorite anyway
+Just some Desktop and icon configuration, my favorite anyway  
+
+# Run this
+./install.sh  
+Then press Alt + F2 then enter r  
+
+Open gnome-tweak  
+Select your theme in gnome-tweak  
---
+++ install.sh
@@ -0,0 +1,6 @@
+mv gnomeshell-theme/Kali-X /usr/share/themes/
+mv gnomeshell-theme/Kali-X-Dark /usr/share/themes/
+mv icon-theme/Vibrancy-Kali /usr/share/icons/
+mv icon-theme/Vibrancy-Kali-Dark /usr/share/icons/
+mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
+apt install gnome-tweaks
