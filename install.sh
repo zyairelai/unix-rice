cd4151f4dc531885a5be4913bd6d94d6baa3c0f4
louiszhenyean <louiszhenyean@gmail.com>
2019-12-15 18:50:07 +0800

    add a dash

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 mv ~/usr/bin/python3 ~/usr/bin/python
 
-mv gnomeshell-theme/Kali-X /usr/share/themes/
-mv gnomeshell-theme/Kali-X-Dark /usr/share/themes/
+mv gnome-shell-theme/Kali-X /usr/share/themes/
+mv gnome-shell-theme/Kali-X-Dark /usr/share/themes/
 mv icon-theme/Vibrancy-Kali /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Dark /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
