bddf31cd38d2466a786265ae0ff98460e25ca240
Yean <louiszhenyean@gmail.com>
2020-05-13 11:18:03 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,10 @@
-mv ~/usr/bin/python3 ~/usr/bin/python
-
 mv gnome-shell-theme/Kali-X /usr/share/themes/
 mv gnome-shell-theme/Kali-X-Dark /usr/share/themes/
 mv icon-theme/Vibrancy-Kali /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Dark /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
 
-apt install git tree pdftk python3-pip 
+apt install git tree pdftk 
           \ screenfetch gnome-tweaks
           \ net-tools nmap wireshark
 apt remove texlive*
