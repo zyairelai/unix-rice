3ebdd5c2db6e244edb73ceba88919bd4955feebc
Yean <louiszhenyean@gmail.com>
2020-05-15 20:03:13 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,16 @@ mv icon-theme/Vibrancy-Kali /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Dark /usr/share/icons/
 mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
 
-apt install git tree pdftk 
-          \ screenfetch gnome-tweaks
-          \ net-tools nmap wireshark
+# PDFtk is to split and merge PDF files
+sudo apt install pdftk
+
+# For Customization
+sudo apt install tree
+sudo apt install screenfetch
+sudo apt install gnome-tweaks
+sudo apt install net-tools
+
+# For Development
+sudo apt install git  
+sudo apt install nmap 
+sudo apt install wireshark
