b25630781f35597c07ceb1e6157c373aabc64822
Yean <louiszhenyean@gmail.com>
2020-05-15 19:53:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,16 +7,3 @@ mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
 apt install git tree pdftk 
           \ screenfetch gnome-tweaks
           \ net-tools nmap wireshark
-
-echo "#hackthebox" >> ~/.bashrc 
-echo "export hackthebox=/home/zyaire/Desktop/hackthebox" >> ~/.bashrc 
-echo "export htb=$hackthebox" >> ~/.bashrc 
-echo "" >> ~/.bashrc
-
-mkdir /home/zyaire/Desktop/git
-mkdir /home/zyaire/Desktop/git/MyProject
-echo "#GitHub" >> ~/.bashrc
-echo "export github=/home/zyaire/Desktop/git" >> ~/.bashrc
-echo "export git=$github" >> ~/.bashrc
-echo "export myProject=/home/zyaire/Desktop/git/MyProject" >> ~/.bashrc
-echo "export me=$myProject" >> ~/.bashrc
