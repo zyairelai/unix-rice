856d26c61dec39456947802c49e43adb9ee3b0a0
Yean <louiszhenyean@gmail.com>
2020-05-15 20:27:43 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,14 +7,17 @@ mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
 # PDFtk is to split and merge PDF files
 sudo apt install pdftk
 
-# For Customization
-sudo apt install gnome-tweaks-tool
-sudo apt install tree
-sudo apt install screenfetch
+# MUST CHECK
 sudo apt install net-tools
-
-# For Development
 sudo apt install git  
 sudo apt install curl
+sudo apt install tree
+sudo apt install screenfetch
+
+# For Customization
+sudo apt install gnome-tweaks-tool
+sudo apt install fonts-powerline
+
+# For Development
 sudo apt install nmap 
 sudo apt install wireshark
