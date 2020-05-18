0592dbd73d7983631ec2696fd66c260768153ba1
Yean <louiszhenyean@gmail.com>
2020-05-18 19:47:38 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,21 +6,16 @@ mkdir /home/zyaire/Desktop/git/MyProject
 git clone https://github.com/louiszhenyean/hackthebox /home/zyaire/Desktop/hackthebox
 ln -s /home/zyaire/Desktop/git ~/git
 ln -s /home/zyaire/Desktop/hackthebox ~/hackthebox
-cp hardcoding.jpg /home/zyaire/Pictures/
+cp hardcoding.jpg /usr/share/backgrounds/
 
 # For Customization
-sudo apt install chrome-gnome-shell gnome-tweak-tool fonts-powerline screenfetch tree
+sudo apt install chrome-gnome-shell gnome-tweak-tool fonts-powerline screenfetch tree zsh nmap
 
 # Installing Zsh
-sudo apt install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 
 # For Development
-sudo apt install nmap wireshark
 sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
 sed 's|path_array+=(.*)|path_array+=("/opt/exploitdb")|g' /opt/exploitdb/.searchsploit_rc > ~/.searchsploit_rc
 sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
-
-# PDFtk is to split and merge PDF files
-# sudo apt install pdftk
