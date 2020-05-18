a4cd76237c534e65a4c5964829790c75cd55e4bf
Yean <louiszhenyean@gmail.com>
2020-05-18 13:31:32 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,25 +1,32 @@
-mv gnome-shell-theme/Kali-X /usr/share/themes/
-mv gnome-shell-theme/Kali-X-Dark /usr/share/themes/
-mv icon-theme/Vibrancy-Kali /usr/share/icons/
-mv icon-theme/Vibrancy-Kali-Dark /usr/share/icons/
-mv icon-theme/Vibrancy-Kali-Full-Dark /usr/share/icons/
-
-mkdir /home/zyaire/Desktop/git
-mkdir /home/zyaire/Desktop/git/MyProject
-ln -s /home/zyaire/Desktop/git ~/git
-ln -s /home/zyaire/Desktop/hackthebox ~/hackthebox
-
-# PDFtk is to split and merge PDF files
-sudo apt install pdftk
-
 # MUST CHECK
 sudo cp /bin/xdg-open /bin/open
 sudo apt install git curl tree screenfetch
 
+cp hardcoding.jpg /home/zyaire/Pictures/
+mkdir /home/zyaire/Desktop/git
+mkdir /home/zyaire/Desktop/git/MyProject
+git clone https://github.com/louiszhenyean/hackthebox /home/zyaire/Desktop/hackthebox
+ln -s /home/zyaire/Desktop/git ~/git
+ln -s /home/zyaire/Desktop/hackthebox ~/hackthebox
+
 # For Customization
-sudo apt install gnome-tweaks-tool
+sudo apt install chrome-gnome-shell
+sudo apt install gnome-tweak-tool
 sudo apt install fonts-powerline
 
+# Installing Zsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+# ~/.zshrc. Set ZSH_THEME="powerlevel9k/powerlevel9k" 
+chsh -s $(which zsh)
+
+
 # For Development
-sudo apt install nmap 
-sudo apt install wireshark
+sudo apt install nmap wireshark
+sudo git clone https://github.com/offensive-security/exploitdb.git /opt/exploitdb
+sed 's|path_array+=(.*)|path_array+=("/opt/exploitdb")|g' /opt/exploitdb/.searchsploit_rc > ~/.searchsploit_rc
+sudo ln -sf /opt/exploitdb/searchsploit /usr/local/bin/searchsploit
+
+# PDFtk is to split and merge PDF files
+sudo apt install pdftk
