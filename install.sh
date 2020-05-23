3dfc5d6b2509554749fa1f51fd7c91a2259179fe
Yean <louiszhenyean@gmail.com>
2020-05-23 23:14:35 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
-cp ~/.bashrc ~/.bashrc.bck
-cp .bashrc ~/.bashrc
-source .bashrc
-
 mkdir /home/zyaire/Desktop/git
 mkdir /home/zyaire/Desktop/git/MyProject
 ln -s /home/zyaire/Desktop/git ~/git
 ln -s /home/zyaire/Desktop/hackthebox ~/hackthebox
-cp hardcoding.jpg /usr/share/backgrounds/
 
-# For Customization
+cp hardcoding.jpg /usr/share/backgrounds/
+cp ~/.bashrc ~/.bashrc.bck
+cp .bashrc ~/.bashrc
+source .bashrc
+
 sudo apt install chrome-gnome-shell gnome-tweak-tool fonts-powerline fonts-noto-color-emoji tree neofetch zsh tmux tilix nmap
