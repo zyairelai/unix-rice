92d21dfbe040aa6c1a75b17b6fe8fe487a7e5d13
Zyaire <louiszhenyean@gmail.com>
2021-04-12 22:42:44 +0800

    Update tmux.sh

--- distros/tmux.sh
+++ distros/tmux.sh
@@ -1,25 +1,15 @@
 echo "Which session do you want to attach?"
-echo "1. core-compounding-BTC"
-echo "2. core-compounding-USD"
-echo "3. futures-omax"
-echo "4. heikin-ashi-5x"
+echo "1. core-compounding"
+echo "2. futures-omax"
 read -p "Enter a number: " userinput
 
 if [ "$userinput" -eq '1' ]
 then
-  tmux a -t core-compounding-BTC
+  tmux a -t core-compounding
 
 elif [ "$userinput" -eq '2' ]
 then
-  tmux a -t core-compounding-USD
-
-elif [ "$userinput" -eq '3' ]
-then
   tmux a -t futures-omax
-
-elif [ "$userinput" -eq '4' ]
-then
-  tmux a -t heikin-ashi-5x
 
 else
   echo "Invalid Number"
