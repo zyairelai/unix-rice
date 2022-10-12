58cc318c235389b7f9313f34589e8328e16abb25
Zyaire <zyairelai@gmail.com>
2022-10-12 19:33:09 +0800

    Update setup.sh

--- kali-xfce/setup.sh
+++ kali-xfce/setup.sh
@@ -34,7 +34,7 @@ chmod a+x rustscan && sudo mv rustscan /usr/bin/
 
 # Installing necessarily tools
 sudo apt update
-sudo apt install tilix python-pip python3-nautilus nautilus neofetch htop totem eog fonts-noto-color-emoji rofi ranger remmina golang-go kali-grant-root kali-root-login -y
+sudo apt install tilix python-pip python3-nautilus nautilus neofetch htop totem eog fonts-noto-color-emoji rofi ranger remmina golang-go mingw-w64 seclists veil kali-grant-root kali-root-login -y
 wget https://download.sublimetext.com/sublime-text_build-3211_amd64.deb
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 wget https://code.visualstudio.com/sha/download\?build=stable\&os=linux-deb-x64 -O vscode.deb
